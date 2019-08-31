c8034235062aa7dd154f3f15a9549e8d79b8227e
hi_go <resonantia@clutte.red>
2019-09-01 07:07:50 +0900

    implemented auto-save and template

--- main.js
+++ main.js
@@ -13,14 +13,14 @@ Vue.component('schedule-item', {
   <li :id = "schedule.id">
   <input type="time" v-model="schedule.start" v-bind:disabled="!isTopItem" title = "開始時刻" /> - 
   <input type="time" v-model="schedule.end" @change = "$emit('change-end-time')" title = "終了時刻" /> 
-  <input type="button" @click = "$emit('insert-now-time')" value = "⌚" title = "現在時刻を挿入（5分刻み）" /> 
+  <input type="button" @click = "$emit('insert-now-time')" class="menu" value = "⌚" title = "現在時刻を挿入（5分刻み）" /> 
   <input type="text" v-model = "schedule.text" @change = "$emit('save-item')" title = "内容" />
-  <item type="time" readonly="readonly" title = "経過時間" />
-  <input type = "button" @click="$emit('add-item')" value= "➕" title ="下に項目を追加" />
-  <input type = "button" @click="$emit('copy-item')" value= "📋" title = "下に内容をコピーして項目を追加" />
-  <input type = "button" @click="$emit('move-up-item')" value= "🔼" title = "この内容を上に移動" v-visible="!isTopItem" />
-  <input type = "button" @click="$emit('move-down-item')" value= "🔽" title = "この内容を下に移動" v-visible="!isBottomItem" />
-  <input type = "button" @click="$emit('delete-item')" value= "❌" title = "この項目を削除" v-visible="isDeletableItem" />
+  <!-- <item type="time" readonly="readonly" title = "経過時間" /> -->
+  <input type = "button" @click="$emit('add-item')" class="menu" value= "➕" title ="下に項目を追加" />
+  <input type = "button" @click="$emit('copy-item')" class="menu" value= "📋" title = "下に内容をコピーして項目を追加" />
+  <input type = "button" @click="$emit('move-up-item')" class="menu" value= "🔼" title = "この内容を上に移動" v-visible="!isTopItem" />
+  <input type = "button" @click="$emit('move-down-item')" class="menu" value= "🔽" title = "この内容を下に移動" v-visible="!isBottomItem" />
+  <input type = "button" @click="$emit('delete-item')" class="menu" value= "❌" title = "この項目を削除" v-visible="isDeletableItem" />
   </li>
   `,
   data: function(){
@@ -57,9 +57,22 @@ var vm = new Vue({
       scheduleList: [
         {id: buildId(), start: '', end: '', duration: '', text: ''}
       ],
-      outputStr: ''
+        outputStr: '',
+      
     },
     methods: {
+        clearList: function () {
+            if ( ! window.confirm('リストをクリアします。よろしいですか？')) { return; }
+            this.scheduleList = [{ id: buildId(), start: '', end: '', duration: '', text: '' }];
+        },
+        saveTemplate: function () {
+            if (!window.confirm('現在のリストをテンプレートとして保存します。よろしいですか？')) { return; }
+            localStorage.scheduleListTemplate = JSON.stringify(this.scheduleList);
+        },
+        loadTemplate: function () {
+            if (!window.confirm('テンプレートをロードして現在のリストを上書きします。よろしいですか？')) { return; }
+            this.scheduleList = JSON.parse(localStorage.scheduleListTemplate);
+        },
         addItem: function(index){
             this.scheduleList.splice(index+1, 0,
             {id: buildId(), start: this.scheduleList[index].end, end: '', text: ''}
@@ -81,7 +94,8 @@ var vm = new Vue({
             this.scheduleList[index].text = this.scheduleList[index+1].text;
             this.scheduleList[index+1].text = tempTxt;
         },
-        deleteItem: function(index){
+        deleteItem: function (index) {
+            if (!window.confirm('アイテムを削除します。よろしいですか？')) { return; }
             if( ! this.$refs[index]['0'].isTopItem &&
                 ! this.$refs[index]['0'].isBottomItem ){
                 this.scheduleList[index+1].start = this.scheduleList[index-1].end;
@@ -99,15 +113,15 @@ var vm = new Vue({
             var nowTime = new Date;
             var hour = nowTime.getHours();
             var min = nowTime.getMinutes();
-            if(min % 10 >= 5){
-                    min += 10;
+            if(min % 5 >= 3){
+                    min += 5;
                     if(min >= 60){
                         hour++;
                         hour %= 24;
                         min %= 60;
                     }
             }
-            min -= min % 10;
+            min -= min % 5;
             var hourStr = ("0" + hour.toString()).slice(-2);
             var minStr = ("0" + min.toString()).slice(-2);
             this.scheduleList[index].end = hourStr + ":" + minStr;
@@ -125,7 +139,6 @@ var vm = new Vue({
                              " - " + this.scheduleList[item].end + " " +
                              this.scheduleList[item].text + "\n";
             }
-            console.log(this.outputStr);
         },
         outputJSON: function(){
             this.outputStr = JSON.stringify(this.scheduleList);
