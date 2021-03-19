e65ca0c4989d96f7a71a4a365bee5d4429411606
hi_go <me@clutte.red>
2021-03-19 22:59:21 +0900

    Format.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <h1>実績入力</h1>
-    <schedule-list
-    id="schedule"
-    ></schedule-list>
+    <schedule-list id="schedule"></schedule-list>
   </div>
 </template>
 <script>
@@ -11,7 +9,7 @@ import ScheduleList from "./components/ScheduleList.vue";
 export default {
   name: "app",
   components: {
-    ScheduleList
-  }
-}
+    ScheduleList,
+  },
+};
 </script>
