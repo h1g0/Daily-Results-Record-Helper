f6b5d13d6582415adc8fac188e4d8ed6e3d56402
hi_go <me@clutte.red>
2021-03-24 22:55:58 +0900

    Add style for `App.vue`.

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,13 @@ export default {
   },
 };
 </script>
+<style scoped>
+h1 {
+  font-size: 16px;
+  padding: 0.3em 0.3em;
+  color: #494949;
+  background: #f4f4f4;
+  border-left: solid 16px #7db4e6;
+  border-bottom: solid 3px #d7d7d7;
+}
+</style>
